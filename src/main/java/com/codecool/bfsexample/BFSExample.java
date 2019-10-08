0e6a281b4dad10df814a243830306531403132c8
SAno <sano1973@hotmail.com>
2019-10-08 11:14:18 +0200

    init a new repo to Peti

---
+++ src/main/java/com/codecool/bfsexample/BFSExample.java
@@ -0,0 +1,22 @@
+package com.codecool.bfsexample;
+
+import com.codecool.bfsexample.model.UserNode;
+import java.util.List;
+
+public class BFSExample {
+
+    private static void populateDB() {
+
+        RandomDataGenerator generator = new RandomDataGenerator();
+        List<UserNode> users = generator.generate();
+
+        GraphPlotter graphPlotter = new GraphPlotter(users);
+
+        
+        System.out.println("Done!");
+    }
+
+    public static void main(String[] args) {
+        populateDB();
+    }
+}
